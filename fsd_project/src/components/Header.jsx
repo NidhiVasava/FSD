503af0640657fb3f6be2436da008be02c7348ff9
Harsh Shah <shahharsh056@gmail.com>
2024-02-15 12:00:08 +0530

    Initial commit and added pages on frontend

---
+++ fsd_project/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+import {AppBar, Toolbar} from '@mui/material';
+
+const Header = () => {
+    // const logoP="https://dashboard.getinvoice.co/dboard/img/logo.png"
+    return(
+        <AppBar >
+            {/* <Toolbar>
+                <img src={logoP} alt="logoP" style={{ width: 120 }}/>
+            </Toolbar>
+            position="static" color="secondary" sx={{ backgroundColor: '#ffc107' }} */}
+        </AppBar>
+       
+    )
+}
+
+export default Header;
